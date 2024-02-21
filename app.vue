11dd518e88c1c394d2571b27c8bfb9f8b03c5fb2
Rodrigo Hernando <rhernandog@gmail.com>
2024-02-21 17:53:22 -0300

    Initial commit

---
+++ app.vue
@@ -0,0 +1,12 @@
+<script setup>
+  import gsap from "gsap";
+  import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+  gsap.registerPlugin(ScrollTrigger);
+</script>
+
+<template>
+  <main>
+    <NuxtPage />
+  </main>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<script setup>
+  import { onMounted } from "vue";
+  import gsap from "gsap";
+  import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+  onMounted(() => {
+    gsap.to(".box", {
+      rotation: 360,
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".gradient-blue",
+        start: "top top",
+        end: "+=200%",
+        pin: true,
+        scrub: true,
+        markers: true,
+      }
+    })
+  });
+</script>
+
+<template>
+  <section class="panel center gradient-green"></section>
+  <section class="panel center gradient-blue">
+    <div class="box">Box</div>
+  </section>
+  <section class="panel center gradient-orange"></section>
+</template>
+
+<style scoped>
+
+</style>
